c64b21ac7407e700b2933bc7c4d1151d61c422cf
Roee Oscar <roee.oscar@live.biu.ac.il>
2021-04-01 14:12:19 +0300

    Add project files.

---
+++ Advanced-Programming-2/MediaPlayer/MediaPlayerView/MediaPlayer.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Advanced_Programming_2.Controls
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class MediaPlayer : UserControl
+    {
+
+        public MediaPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void V_ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void V_BackwardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    
+
+        private void V_StopButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Advanced-Programming-2/Model/FlightAnalysisModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+
+namespace Advanced_Programming_2.Model
+{
+    class FlightAnalysisModel : IFlightAnalysisModel
+    {
+        public static void communicateWithFG()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 5400);
+
+            StreamWriter streamWriter = new StreamWriter(client.GetStream());
+            StreamReader streamReader = new StreamReader("C:/Users/roee0/source/repos/Advanced-Programming-2/Advanced-Programming-2/Model/reg_flight.csv");
+
+            string strline = "";
+
+
+            while (!streamReader.EndOfStream)
+            {
+                strline = streamReader.ReadLine();
+                byte[] bytesData = System.Text.Encoding.ASCII.GetBytes(strline);
+                streamWriter.WriteLine(strline);
+                Thread.Sleep(100);
+            }
+            streamWriter.Close();
+            streamReader.Close();
+
+        }
+
+
+    }
+
+}
