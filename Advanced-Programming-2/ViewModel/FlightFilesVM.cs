cb97b783c0246fee3ae3a67642ffab048704a6e9
Michael Albo <mickyloulou@gmail.com>
2021-04-01 19:02:20 +0300

    add FlightFilesVM

---
+++ Advanced-Programming-2/ViewModel/FlightFilesVM.cs
@@ -0,0 +1,26 @@
+class FlightFilesVM
+{
+    //IFlightAnalysisModel model;
+    public string VM_csvFileName
+    {
+        get
+        {
+            return "";
+        }
+        set
+        {
+
+        }
+    }
+    public string VM_xmlFileName
+    {
+        get
+        {
+            return "";
+        }
+        set
+        {
+
+        }
+    }
+}
