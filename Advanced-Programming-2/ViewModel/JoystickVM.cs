953b440d01bc0e1de24aa8524c03e0be58a303f0
Roee Oscar <roee.oscar@live.biu.ac.il>
2021-04-05 17:06:25 +0300

    Adding joystick.

---
+++ Advanced-Programming-2/ViewModel/JoystickVM.cs
@@ -0,0 +1,36 @@
+﻿using Advanced_Programming_2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Programming_2.ViewModel
+{
+    class JoystickVM : INotifyPropertyChanged
+    {
+        private IFlightAnalysisModel model;
+        public JoystickVM(IFlightAnalysisModel model)
+        {
+            this.model = model;
+            this.model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                {
+                    NotifyPropertyChanged("VM_" + e.PropertyName);
+
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
