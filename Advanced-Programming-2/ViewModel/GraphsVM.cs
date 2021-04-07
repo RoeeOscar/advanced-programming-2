5d56b77c0a753b3600ff988cf9a813ee7f82b81d
Roee Oscar <roee.oscar@live.biu.ac.il>
2021-04-07 10:30:03 +0300

    Graphs view model.

---
+++ Advanced-Programming-2/ViewModel/GraphsVM.cs
@@ -0,0 +1,35 @@
+﻿using Advanced_Programming_2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Programming_2.ViewModel
+{
+    public class GraphsVM : INotifyPropertyChanged
+    {
+        private IFlightAnalysisModel model;
+        public GraphsVM(IFlightAnalysisModel model)
+        {
+            this.model = model;
+            this.model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                {
+                    NotifyPropertyChanged("VM_" + e.PropertyName);
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
