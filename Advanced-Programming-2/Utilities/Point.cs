28a9e7c3335105d9ebcb590fb555978cb8e78f66
Roee Oscar <roee.oscar@live.biu.ac.il>
2021-04-10 12:11:19 +0300

    Adding Point Class.

---
+++ Advanced-Programming-2/Utilities/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Programming_2.Utilities
+{
+    public class Point
+    {
+        double x, y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+
+    }
+}
