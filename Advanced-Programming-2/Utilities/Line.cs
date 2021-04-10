65f21640a863498daf90bebbe433c6e6276f8ce3
Roee Oscar <roee.oscar@live.biu.ac.il>
2021-04-10 12:11:07 +0300

    Adding Line class.

---
+++ Advanced-Programming-2/Utilities/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Programming_2.Utilities
+{
+    public class Line
+    {
+        double a, b;
+        
+        public Line()
+        {
+            a = 0;
+            b = 0;
+        }
+        public Line(double a, double b)
+        {
+            this.a = a;
+           this.b = b;
+        }
+
+        public double f(double x)
+        {
+            return a * x + b;
+        }
+    }
+}
