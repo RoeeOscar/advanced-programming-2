3a1af9f91825aabb7938b77459b1d1659859303d
Roee Oscar <roee.oscar@live.biu.ac.il>
2021-04-10 12:10:54 +0300

    Adding regression line and min and max points.

--- Advanced-Programming-2/Utilities/Correlations.cs
+++ Advanced-Programming-2/Utilities/Correlations.cs
@@ -16,7 +16,7 @@ namespace Advanced_Programming_2.Utilities
             foreach (string key in values.Keys)
             {
                 string maxCorrelationAttribute = null;
-                double maxCorrelation = 0;
+                double maxCorrelation = -1;
                 foreach(string otherKey in values.Keys)
                 {
                     if (key != otherKey)
@@ -29,12 +29,31 @@ namespace Advanced_Programming_2.Utilities
                         }
                     }
                 }
-                if (maxCorrelation > 0)
+                if (maxCorrelation >= 0)
                 {
                     CorrelatedFeatures correlatedFeatures = new CorrelatedFeatures();
                     correlatedFeatures.setFeature1(key);
                     correlatedFeatures.setFeature2(maxCorrelationAttribute);
                     correlatedFeatures.setCorrelation(maxCorrelation);
+                    correlatedFeatures.setRegressionLine(AnomalyDetectionUtil.linearRegression(values[key], values[maxCorrelationAttribute]));
+               //     Console.WriteLine(key + "  to  " + maxCorrelationAttribute);
+               //     Console.WriteLine("the pearson is: " + maxCorrelation);
+                    double maxX = values[key][0];
+                    double minX = values[key][0];
+                    for (int i = 1; i < values[key].Count(); i++)
+                    {
+                        if (values[key][i] > maxX)
+                        {
+                            maxX = values[key][i];
+                        }
+                        if (values[key][i] < minX)
+                        {
+                            minX = values[key][i];
+                        }
+                    }
+                    correlatedFeatures.setMinXY(new Point(minX, correlatedFeatures.getRegressionLine().f(minX)));
+                    correlatedFeatures.setMaxXY(new Point(maxX, correlatedFeatures.getRegressionLine().f(maxX)));
+
                     cf.Add(correlatedFeatures);
                 }
             }
